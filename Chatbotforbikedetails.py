653b5aa18b0e6af06c42423806c7e0c6db8a3c57
Anbarasan A <79267546+anbarsan26@users.noreply.github.com>
2022-02-09 14:03:36 +0530

    Add files via upload

---
+++ Chatbotforbikedetails.py
@@ -0,0 +1,72 @@
+import io
+import random
+import string
+import warnings
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+warnings.filterwarnings('ignore')
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+nltk.download('popular', quiet=True)
+
+with open('chatbotdoc.txt','r', encoding='utf8', errors ='ignore') as fin:
+    raw = fin.read().lower()
+
+sent_tokens = nltk.sent_tokenize(raw)
+word_tokens = nltk.word_tokenize(raw)
+
+lemmer = WordNetLemmatizer()
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+GREETING_INPUTS = ("hello", "hi", "greetings", "what's up","hey",)
+GREETING_RESPONSES = ["hi", "hey", "hi there", "hello", "I am glad! You are talking to me"]
+
+def greeting(sentence):
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+def response(user_response):
+    parker_response=''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        parker_response=parker_response+"I am sorry! I don't understand you"
+        return parker_response
+    else:
+        parker_response = parker_response+sent_tokens[idx]
+        return parker_response
+
+
+flag=True
+print("Hi My name is Paker, I will answer your queries about bikes. If you want to exit, type Bye!")
+while(flag==True):
+    user_response = input()
+    user_response=user_response.lower()
+    if(user_response!='bye'):
+        if(user_response=='thanks' or user_response=='thank you' ):
+            flag=False
+            print("PARKER: You are welcome..")
+        else:
+            if(greeting(user_response)!=None):
+                print("PARKER: "+greeting(user_response))
+            else:
+                print("PARKER: ",end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag=False
+        print("PARKER:Thanks for getting me!!")    
